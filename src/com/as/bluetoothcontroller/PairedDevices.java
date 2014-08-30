6767a21d4ec1751765fd121ac0a892718f318339
ashokgujju <ashokg.geek@gmail.com>
2014-08-30 07:48:48 +0530

    send data to paired device

--- src/com/as/bluetoothcontroller/PairedDevices.java
+++ src/com/as/bluetoothcontroller/PairedDevices.java
@@ -1,12 +1,17 @@
 package com.as.bluetoothcontroller;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Set;
 
 import android.annotation.SuppressLint;
 import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -20,6 +25,7 @@ public class PairedDevices extends ListActivity {
 	ArrayAdapter<String>  adapter;
 	BluetoothDevice mPairedDevices [];
 	private OutputStream outputStream;
+	String command;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +51,56 @@ public class PairedDevices extends ListActivity {
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
 		
+		command = "100"+(position+1);
 		
 		if(getListView().isItemChecked(position)) {
-			Toast.makeText(PairedDevices.this, "", Toast.LENGTH_LONG).show();
+			Toast.makeText(PairedDevices.this, command+"03", Toast.LENGTH_LONG).show();
+			command = command+"03";
 		}
+		else {
+			Toast.makeText(PairedDevices.this, command+"02", Toast.LENGTH_LONG).show();
+			command = command+"02";
+		}
+		sendData(position);
+	}
+	
+	OutputStream os;
+	
+	public void sendData(int position) {
+		BluetoothDevice device = mPairedDevices[position];
+		 try {
+			Method m = device.getClass().getMethod("createRfcommSocket", new Class[] {int.class});
+			BluetoothSocket clientSocket =  (BluetoothSocket) m.invoke(device, 1);
+			clientSocket.connect();
+			os = new DataOutputStream(clientSocket.getOutputStream());
+			new clientSock().start();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public class clientSock extends Thread {
+	    public void run () {
+	        try {
+	            os.write(command.getBytes()); 
+	            os.flush();
+	        } catch (Exception e1) {
+	            e1.printStackTrace();
+	            return;
+	        }
+	    }
 	}
 }
